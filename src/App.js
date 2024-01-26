d394af1f96acd447a608ef7cd401f15e87c49fa7
The Margad <122777402+TheMargadXR@users.noreply.github.com>
2024-01-26 15:42:30 +0800

    cmd check

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          check <code>src/App.js</code> and save to reload.
+          git cmd <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
